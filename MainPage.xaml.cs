0073bebaca5ad04a42dd7dbbc6a5a3630f50671a
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-05-25 16:59:12 +0300

    The xaml file of the view
    "Here is the .xaml file with the view of the mobile application which is the only one here!

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+    using Calcucator.Model;
+using Calcucator.ViewModel;
+using CommunityToolkit.Mvvm.Input;
+
+    namespace Calcucator.Views;
+
+    public partial class MainPage : ContentPage
+    {
+	    public MainPage()
+	    {
+		    InitializeComponent();
+        }
+
+
+
+    }
