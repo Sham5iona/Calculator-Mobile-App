e1e1754358e58c511ce5c291f9f08f271b42705a
Sham5iona <gom26@abv.bg>
2024-05-25 16:39:43 +0300

    This is the model of the mobile app!

---
+++ MainPageModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcucator.Model
+{
+    public class MainPageModel
+    {
+        public MainPageModel()
+        {
+            
+        }
+        public double Solve(string expression)
+        {
+            
+            double result = 0.0;
+            result = Convert.ToDouble(new DataTable().Compute(expression, null));
+            return result;
+        }
+    }
+}
